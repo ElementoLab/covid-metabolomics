6c7433dd92ef8c0d4ea9d4ca168e88834e908e43
afrendeiro <afrendeiro@gmail.com>
2021-05-25 19:31:24 -0400

    more strict filtering of missing data in takahashi dataset; visualize patient trajectories; use new DataSet class

--- src/takahashi.py
+++ src/takahashi.py
@@ -13,19 +13,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from imc.types import DataFrame, Array
+from imc.types import DataFrame
 from imc.graphics import close_plots
 from seaborn_extensions import clustermap, swarmboxenplot
 
 from src.config import data_dir, metadata_dir, results_dir, figkws
-from src.analysis import plot_projection
+from src.models import DataSet
+from src.ops import unsupervised, overlay_individuals_over_global
 
 
-attributes = ["Age", "Sex", "BMI", "DFSO", "ICU", "Clinical_score"]
+attributes = ["Age", "Sex", "BMI", "DFSO", "ICU", "Clinical_score", "Outcome"]
 palettes = dict(
     Sex=sns.color_palette("Pastel1")[3:5],
     ICU=[(0.1, 0.1, 0.1)] + sns.color_palette("Set2")[:2],
     Clinical_score=np.asarray(sns.color_palette("Set1"))[[2, 1, 7, 3, 4, 0]].tolist(),
+    WHO_score_sample=np.asarray(sns.color_palette("Set1"))[[2, 1, 7, 3, 4, 0]].tolist(),
+    Outcome=np.asarray(sns.color_palette("Set1"))[[3, 4, 0, 2]].tolist(),
 )
 cmaps = dict(
     Age="winter_r",
@@ -37,20 +40,36 @@ output_dir = (results_dir / "takahashi").mkdir()
 
 def main() -> int:
     """The main function to run the analysis."""
+    d = get_dataset()
 
-    x, y = get_suppl_data()
+    summary_plots(d)
 
-    summary_plots(x, y)
+    d.x = impute_x(np.log1p(d.x), frac_obs=0.25, frac_var=0.3)
+    d.obs = d.obs.reindex(index=d.x.index)
+    d.var = d.var.reindex(index=d.x.columns)
 
-    x_imp = impute_x(np.log1p(x))
+    unsupervised(d.x, d.obs, attributes=attributes, data_type="takahashi")
+    unsupervised2(d.x, d.obs, attributes)
 
-    unsupervised(x_imp, y.reindex(x_imp.index), attributes)
+    overlay_individuals_over_global(d.x, d.obs, data_type="takahashi")
+
+    sel = d.obs.index.str.startswith("Pt")
+    unsupervised(
+        d.x.loc[sel],
+        d.obs.loc[sel],
+        attributes=attributes,
+        data_type="takahashi",
+        suffix="only_patients",
+    )
+    overlay_individuals_over_global(
+        d.x.loc[sel], d.obs.loc[sel], data_type="takahashi", suffix="only_patients"
+    )
 
     # Fin
     return 0
 
 
-def get_suppl_data() -> tp.Tuple[DataFrame, DataFrame]:
+def get_dataset() -> DataSet:
     """
     Download and prepare data from supplementary material of original manuscript.
     """
@@ -93,25 +112,59 @@ def get_suppl_data() -> tp.Tuple[DataFrame, DataFrame]:
     y = y.assign(Sex=y["sex"]).drop("sex", 1)
     y["ICU"] = y["ICU"].astype(pd.CategoricalDtype())
 
-    return x, y
+    # for compatibility with NMR data:
+    date_diagnosis = pd.to_datetime("2020/02/01")
+    y["date_sample"] = date_diagnosis + pd.to_timedelta(y["DFSO"], unit="day")
+    y["patient_code"] = y["individual_id"]
+    y["accession"] = y.index
+    y["WHO_score_sample"] = y["Clinical_score"]
+
+    # The supplementary material from the Lucas et al paper has one more column of interest:
+    root = URL(
+        "https://static-content.springer.com/esm/art%3A10.1038%2Fs41586-020-2588-y"
+    )
+    url = root / "MediaObjects/41586_2020_2588_MOESM3_ESM.xlsx"
+    df2 = pd.read_excel(url, index_col=0, skiprows=26)
+    y["Outcome"] = pd.Categorical(
+        y.join(df2["LatestOutcome"])["LatestOutcome"].replace(
+            {0: "still admitted", 1: "discharged", 2: "deceased", 3: "CMO/hospice"}
+        ),
+        ordered=True,
+        categories=["still admitted", "CMO/hospice", "deceased", "discharged"],
+    )
+    # # for compatibility with NMR data:
+    y["alive"] = y["Outcome"]
+
+    return DataSet(
+        x=x,
+        obs=y,
+        var=var,
+        attributes=attributes,
+        metadata_dir=metadata_dir,
+        data_dir=data_dir,
+        results_dir=results_dir,
+        palettes=palettes,
+        cmaps=cmaps,
+    )
 
 
 def get_feature_annotations(x: DataFrame) -> DataFrame:
     import json
 
-    var_annot = json.load(open(metadata_dir / "takahashi.variable_classes.json"))
+    with open(metadata_dir / "takahashi.variable_classes.json") as handle:
+        var_annot = json.load(handle)
     var = pd.Series({x: k for k, v in var_annot.items() for x in v}, name="feature_type")
     return var.to_frame().reindex(x.columns).astype(pd.CategoricalDtype())
 
 
 @close_plots
-def summary_plots(x: DataFrame, y: DataFrame) -> None:
+def summary_plots(d: DataSet) -> None:
 
     # Clinical data
     fig, axes = plt.subplots(1, 2, figsize=(2 * 3, 2))
-    sns.histplot(y.groupby("individual_id").size(), ax=axes[0])
+    sns.histplot(d.obs.groupby("individual_id").size(), ax=axes[0])
     sns.histplot(
-        y.query("patient == 'COVID-19'").groupby("individual_id").size(), ax=axes[1]
+        d.obs.query("patient == 'COVID-19'").groupby("individual_id").size(), ax=axes[1]
     )
     axes[0].set(title="All individuals", xlabel="Number of samples", ylabel="Frequency")
     axes[1].set(title="COVID-19 patients", xlabel="Number of samples", ylabel="Frequency")
@@ -120,9 +173,9 @@ def summary_plots(x: DataFrame, y: DataFrame) -> None:
     )
 
     fig, axes = plt.subplots(1, 2, figsize=(2 * 3, 2))
-    sns.histplot(y["DFSO"], ax=axes[0])
+    sns.histplot(d.obs["DFSO"], ax=axes[0])
     sns.histplot(
-        y.query("patient == 'COVID-19'").groupby("individual_id")["DFSO"].diff(),
+        d.obs.query("patient == 'COVID-19'").groupby("individual_id")["DFSO"].diff(),
         ax=axes[1],
     )
     axes[0].set(xlabel="Days from symptom onset", ylabel="Frequency")
@@ -132,7 +185,7 @@ def summary_plots(x: DataFrame, y: DataFrame) -> None:
     fig, axes = plt.subplots(3, 3, figsize=(3 * 4, 3 * 4))
     factors = ["Sex", "Clinical_score", "ICU"]
     for factor, axs in zip(factors, axes.T):
-        swarmboxenplot(data=y, x=factor, y=["DFSO", "BMI", "Age"], ax=axs)
+        swarmboxenplot(data=d.obs, x=factor, y=["DFSO", "BMI", "Age"], ax=axs)
     for ax, label in zip(axes[-1], factors):
         ax.set(xlabel=label)
     for ax in axes[0]:
@@ -142,12 +195,12 @@ def summary_plots(x: DataFrame, y: DataFrame) -> None:
     )
 
     # Data
-    var = get_feature_annotations(x)
+    var = get_feature_annotations(d.x)
 
     # # Missing data
     fig, axes = plt.subplots(1, 2, figsize=(2 * 3, 1 * 3), sharex=True)
     for i, (ax, label) in enumerate(zip(axes, ["Feature", "Sample"])):
-        sns.histplot(x.isnull().sum(i) / x.shape[i] * 100, bins=25, ax=ax)
+        sns.histplot(d.x.isnull().sum(i) / d.x.shape[i] * 100, bins=25, ax=ax)
         ax.set(xlabel=f"Missing data per {label} (%)", ylabel="Frequency")
     axes[0].axvline(50, linestyle="--", color="grey")
     axes[1].axvline(80, linestyle="--", color="grey")
@@ -157,23 +210,23 @@ def summary_plots(x: DataFrame, y: DataFrame) -> None:
     fig, axes = plt.subplots(1, 2, figsize=(2 * 4, 1 * 4))
     ax = axes[0]
     for cat in var["feature_type"].unique():
-        _x = x.loc[:, var["feature_type"] == cat]
+        _x = d.x.loc[:, var["feature_type"] == cat]
         ax.scatter(_x.mean(), _x.std(), label=cat, alpha=0.5)
     ax.loglog()
     ax.legend()
-    vmin = min(x.mean().min(), x.std().min())
-    vmax = max(x.mean().max(), x.std().max())
+    vmin = min(d.x.mean().min(), d.x.std().min())
+    vmax = max(d.x.mean().max(), d.x.std().max())
     ax.plot((vmin, vmax), (vmin, vmax), linestyle="--", color="grey")
     ax.set(xlabel="Mean", ylabel="Standard deviation")
 
     ax = axes[1]
     for cat in var["feature_type"].unique():
-        _x = x.loc[:, var["feature_type"] == cat]
+        _x = d.x.loc[:, var["feature_type"] == cat]
         ax.scatter(_x.mean(), _x.std() / _x.mean(), label=cat, alpha=0.5)
     ax.loglog()
     ax.legend()
-    vmin = min(x.mean().min(), x.std().min())
-    vmax = max(x.mean().max(), x.std().max())
+    vmin = min(d.x.mean().min(), d.x.std().min())
+    vmax = max(d.x.mean().max(), d.x.std().max())
     ax.plot((vmin, vmax), (vmin, vmax), linestyle="--", color="grey")
     ax.set(xlabel="Mean", ylabel="Coefficient of variation")
     fig.savefig(output_dir / "summary_plots.data_variance.scatterplot.svg", **figkws)
@@ -200,7 +253,7 @@ def impute_x(
             null.sum(1) / x.shape[1] < frac_obs, null.sum(0) / x.shape[0] < frac_var
         ]
         if method == "factorization":
-            model = MatrixFactorization(learning_rate=0.01, epochs=500)
+            model = MatrixFactorization(learning_rate=0.01, epochs=500, verbose=False)
         elif method == "knn":
             model = KNN(15)
         x_imp = pd.DataFrame(
@@ -211,102 +264,31 @@ def impute_x(
         # clip variables to zero
         if save:
             x_imp.clip(lower=0).to_csv(x_file)
+    else:
+        print("Using pre-exising dataset.")
     x_imp = pd.read_csv(x_file, index_col=0)
 
     return x_imp
 
 
 @close_plots
-def unsupervised(x: DataFrame, y: DataFrame, factors: tp.Sequence[str]) -> None:
-    from imc.utils import z_score
-
-    from sklearn.decomposition import PCA, NMF
-    from sklearn.manifold import MDS, Isomap, TSNE, SpectralEmbedding
-    from umap import UMAP
-    from src.config import PyMDE, DiffMap
-
-    var = get_feature_annotations(x)
+def unsupervised2(d: DataSet) -> None:
+    var = get_feature_annotations(d.x)
     grid = clustermap(
-        x, config="z", row_colors=y[factors], col_colors=var, figsize=(20, 12)
+        d.x, config="z", row_colors=d.obs[d.attributes], col_colors=var, figsize=(20, 12)
     )
     grid.fig.savefig(output_dir / "unsupervised.clustermap.svg", **figkws)
 
-    ## Clustermaps
-    for c in ["abs", "z"]:
-        grid = clustermap(
-            x,
-            row_colors=y[factors],
-            col_colors=var,
-            config=c,
-            rasterized=True,
-        )
-        grid.savefig(
-            output_dir / f"unsupervised.clustering.clustermap.{c}.svg",
-            **figkws,
-        )
-        plt.close(grid.fig)
-    kws = dict(
-        cmap="RdBu_r",
-        rasterized=True,
-        cbar_kws=dict(label="Pearson correlation"),
-        xticklabels=False,
-        yticklabels=False,
-    )
-    grid = clustermap(z_score(x).corr(), center=0, **kws, row_colors=var)
-    grid.savefig(
-        output_dir / "unsupervised.correlation_variable.clustermap.svg",
-        **figkws,
-    )
-    plt.close(grid.fig)
 
-    grid = clustermap(z_score(x).T.corr(), **kws, row_colors=y[factors])
-    grid.savefig(
-        output_dir / "unsupervised.correlation_samples.clustermap.svg",
-        **figkws,
-    )
-    plt.close(grid.fig)
-
-    ## Dimres
-    for model, pkwargs, mkwargs in [
-        (PCA, dict(), dict()),
-        (NMF, dict(), dict()),
-        (MDS, dict(n_dims=1), dict()),
-        (TSNE, dict(n_dims=1), dict()),
-        (Isomap, dict(n_dims=1), dict()),
-        (UMAP, dict(n_dims=1), dict(random_state=0)),
-        (DiffMap, dict(n_dims=1), dict()),
-        (PyMDE, dict(n_dims=1), dict()),
-        (SpectralEmbedding, dict(n_dims=1), dict()),
-    ][::-1]:
-        # model, pkwargs, mkwargs = (PyMDE, dict(), dict())
-        name = str(model).split(".")[-1].split("'")[0]
-        model_inst = model(**mkwargs)
-
-        for transf, label in [(lambda x: x, ""), (z_score, "Z-score.")]:
-            try:  #  this will occur for example in NMF with Z-score transform
-                res = pd.DataFrame(model_inst.fit_transform(transf(x)), index=x.index)
-            except ValueError:
-                continue
-
-            fig = plot_projection(
-                res, y.reindex(res.index), factors=factors, algo_name=name, **pkwargs
-            )
-            fig.savefig(
-                output_dir / f"unsupervised.dimres.{name}.{label}svg",
-                **figkws,
-            )
-            plt.close(fig)
-
-
-def supervised(x: DataFrame, y: DataFrame) -> None:
+def supervised(d: DataSet) -> None:
 
     _, stats = swarmboxenplot(
-        data=x.join(y), x="Clinical_score", y=x.columns, swarm=False, boxen=False
+        data=d.x.join(d.obs), x="Clinical_score", y=d.x.columns, swarm=False, boxen=False
     )
     stats.to_csv(output_dir / "supervised.stats.csv")
 
     fig, _stats = swarmboxenplot(
-        data=np.log(x).join(y),
+        data=d.x.join(d.obs),
         x="Clinical_score",
         y=stats.sort_values("p-unc").head(35)["Variable"].unique(),
     )
